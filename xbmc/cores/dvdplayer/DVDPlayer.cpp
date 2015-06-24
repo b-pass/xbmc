545489b94963c59252a20e9de00e13bef51ab9dd
Bryan <b-pass@users.noreply.github.com>
2015-06-24 09:46:51 +0000

    Switched commskipdisable to commskipenable (makes more sense to turn something on than turn it off).  Note it still defaults to TRUE because currently it is enabled and there is no way to turn it off, this just adds a way.
    Moved the settings to video/tv area and updated help a little.

--- xbmc/cores/dvdplayer/DVDPlayer.cpp
+++ xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -580,8 +580,8 @@ CDVDPlayer::CDVDPlayer(IPlayerCallback& callback)
   m_omxplayer_mode                     = false;
 #endif
 
-  m_SkipCommercials = !CSettings::Get().GetBool("pvrplayback.commskipdisabled");
-  m_NotifyCommercials = CSettings::Get().GetBool("pvrplayback.commskipnotify");
+  m_SkipCommercials = CSettings::Get().GetBool("videoplayer.commskipenabled");
+  m_NotifyCommercials = CSettings::Get().GetBool("videoplater.commskipnotify");
 
   CreatePlayers();
 }
