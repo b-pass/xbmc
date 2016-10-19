b44fcba3adfcdb59d23a448f546ef290a38d5796
b-pass <b-pass@users.noreply.github.com>
2016-10-19 10:35:03 -0400

    A new type of list provider, MultiProvider, which allows multiple <content> blocks in a single container.

--- xbmc/listproviders/IListProvider.h
+++ xbmc/listproviders/IListProvider.h
@@ -38,11 +38,18 @@ public:
   virtual ~IListProvider() {}
 
   /*! \brief Factory to create list providers.
-   \param node a TiXmlNode to create.
+   \param parent a parent TiXmlNode for the container.
    \param parentID id of parent window for context.
    \return the list provider, NULL if none.
    */
-  static IListProvider *Create(const TiXmlNode *node, int parentID);
+  static IListProvider *Create(const TiXmlNode *parent, int parentID);
+
+  /*! \brief Factory to create list providers.  Cannot create a multi-provider.
+   \param content the TiXmlNode for the content to create.
+   \param parentID id of parent window for context.
+   \return the list provider, NULL if none.
+   */
+  static IListProvider *CreateSingle(const TiXmlNode *content, int parentID);
 
   /*! \brief Update the list content
    \return true if the content has changed, false otherwise.
@@ -52,7 +59,7 @@ public:
   /*! \brief Fetch the current list of items.
    \param items [out] the list to be filled.
    */
-  virtual void Fetch(std::vector<CGUIListItemPtr> &items) const=0;
+  virtual void Fetch(std::vector<CGUIListItemPtr> &items)=0;
 
   /*! \brief Check whether the list provider is updating content.
    \return true if in the processing of updating, false otherwise.
